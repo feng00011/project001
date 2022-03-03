5875dc58a568186f9c52cbc5d2fe3cacff5a685b
feng <1971949826@qq.com>
2022-03-03 20:50:26 +0800

    完善了拓扑图的细节、

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,94 @@
 
+
+
+
+
+
 //注意：导航 依赖 element 模块，否则无法进行功能性操作
 layui.use('element', function () {
     var element = layui.element;
 
     //…
 });
-$(function(){
-    mouseMove()
+$(function () {
+    parserMod()
 })
 // 测试：拖动div内的元素？后台传过来的是svg，是可以选中的
-function mouseMove(){
-    var moveTarget = $('.my-left');
-    $(moveTarget).on('mousedown',function(e){
-        console.log(e.offsetLeft);
-        console.log(e.pageY);
-        $(moveTarget).css('left',e.offsetLeft+'px')
-    })
-}
 
+
+
+function parserMod() {
+    var dotSrc = `
+digraph {
+    graph [label="Click on a node or an edge to delete it" labelloc="t", fontsize="20.0" tooltip=" "]
+    Node1 [id="NodeId1" label="N1" my1=1]
+    Node2 [id="NodeId2" label="N2" ]
+    Node3 [id="NodeId3" label="N3" ]
+    Node4 [id="NodeId4" label="N4" ]
+    Node1 -> Node2 [id="EdgeId12" label="E12"]
+    Node1 -> Node3 [id="EdgeId13" label="E13"]
+    Node2 -> Node3 [id="EdgeId23" label="E23"]
+    Node3 -> Node4 [id="EdgeId34" label="E34"]
+}
+`;
+    var dotSrcLines;
+    var dotSrc1 = dotSrc;
+    var graphviz = d3.select("#graph").graphviz().width('900px').height('90vh').fit(true);
+    function render() {
+        // console.log('DOT source =', dotSrc);
+        dotSrcLines = dotSrc.split('\n');
+        graphviz
+            .renderDot(dotSrc)
+            .on("end", interactive);
+    }
+
+    function render2() {
+        dotSrcLines = dotSrc1.split('\n');
+        graphviz
+            .renderDot(dotSrc);
+
+    }
+    function interactive() {
+        // console.log(dotSrcLines);
+        nodes = d3.selectAll('.node,.edge');
+        nodes
+            .on("click", function () {
+                render2();
+                var title = d3.select(this).selectAll('title').text().trim();
+                var text = d3.select(this).selectAll('text').text();
+                var id = d3.select(this).attr('id');
+                var class1 = d3.select(this).attr('class');
+                // var reg = new RegExp("a", "g");
+                dotElement = title.replace('->', ' -> ');
+                // 这是针对path部分的，也可以叫edge，title命名规则是a->b，需加空格
+                console.log('Element id="%s" class="%s" title="%s" text="%s" dotElement="%s"', id, class1, title, text, dotElement);
+                console.log('Finding and deleting references to %s "%s" from the DOT source', class1, dotElement);
+                for (i = 0; i < dotSrcLines.length;) {
+                    if (dotSrcLines[i].indexOf(dotElement) >= 0) {
+                        //使得相关分支都变色，所以不能使用title等点击事件获取的当前元素属性
+                        console.log('Changing line %d: %s', i, dotSrcLines[i]);
+                        let idd = dotSrcLines[i].match(/id="(\S*)"/);
+                        let txt = dotSrcLines[i].match(/label="(\S*)"/);
+                        let dotE = dotSrcLines[i].match(/(.+?)\[/g);
+                        dotE = dotE.toString().replace(/\[/, '').trim();
+                        // console.log(dotE);
+                        dotSrcLines[i] = '    ' + dotE + ' [id=' + '"' + idd[1] + '"' + ' label=' + '"' + txt[1] + '"' + ' color="red"]';
+                        console.log(dotSrcLines);
+                        //前进去搜索与其相关的，此时需要另外赋值id，text ，dotElement
+                        i++;
+                    } else {
+                        i++;
+                    }
+                }
+                // console.log(dotSrc);
+
+                dotSrc = dotSrcLines.join('\n');
+                // console.log(dotSrc);
+                render();
+
+            });
+    }
+
+    render();
+
+}
--- copybord.md
+++ copybord.md
@@ -13,3 +13,8 @@
                 </li>
                 <li class="layui-nav-item"><a href="">社区</a></li>
             </ul>
+
+            
+            d3.select("#graph").graphviz().engine('dot')
+                .renderDot('digraph  { table1[label = "<h1>123</h1>"];A->B[color=blue,dir=both,label="双向 "];B->C[dir=none,label="无向 "];C->D[dir=back];D->A[dir=forward];a -> c;b -> f;c ->f;f ->e;a->e;f -> g;a -> g;b -> gsdsdsdsdsdds;c -> g;a -> h;b -> h;f -> h;g -> h;c -> h;e -> h}');
+            d3.select("#graph").graphviz().width('900px').height('90vh').fit(true);
